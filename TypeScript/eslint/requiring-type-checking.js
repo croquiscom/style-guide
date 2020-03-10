bde40932fba1075328bde6963a44b9e0600a56ca
Sangmin Yoon <sangmin.yoon@croquis.com>
2020-03-10 23:12:01 +0900

    @typescript-eslint/prefer-includes 규칙 끔

--- TypeScript/eslint/requiring-type-checking.js
+++ TypeScript/eslint/requiring-type-checking.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
 
   rules: {
+    // indexOf 사용을 허용한다. (클라이언트 코드 때문에)
+    '@typescript-eslint/prefer-includes': 'off',
+
     // unbound method를 허용한다. _.identity 같은 코드가 있다.
     '@typescript-eslint/unbound-method': 'off',
   },
