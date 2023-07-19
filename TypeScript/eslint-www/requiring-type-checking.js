3f6e0403d9ef2c697d42ae4f74cc873aaae93a99
Simon, 윤상민 <sixmen@gmail.com>
2023-07-19 14:22:18 +0900

    Update module (#65)

--- TypeScript/eslint-www/requiring-type-checking.js
+++ TypeScript/eslint-www/requiring-type-checking.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ['./index', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
+  extends: [
+    './index',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+  ],
 
   rules: {
     // indexOf 사용을 허용한다. (클라이언트 코드 때문에)
@@ -7,5 +11,27 @@ module.exports = {
 
     // unbound method를 허용한다. _.identity 같은 코드가 있다.
     '@typescript-eslint/unbound-method': 'off',
+
+    // reoverride rules by plugin:@typescript-eslint/recommended-type-checked
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        ignoreRestSiblings: true,
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
+
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
 };
