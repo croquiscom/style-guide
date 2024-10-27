68473dd47ec154c3f4e61f927d1a5d795ce21b16
Sangmin Yoon <sixmen@gmail.com>
2024-10-27 19:25:10 +0900

    fix: replace undefined import.meta.dirname

--- TypeScript/eslint-www/eslint.config.mjs
+++ TypeScript/eslint-www/eslint.config.mjs
@@ -1,16 +1,22 @@
+import path from 'path';
+import url from 'url';
 import tseslint from 'typescript-eslint';
 import baseConfig from './requiring-type-checking.mjs';
+
+const dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 export default tseslint.config(...baseConfig, {
   languageOptions: {
     parserOptions: {
       projectService: true,
-      tsconfigRootDir: import.meta.dirname,
+      tsconfigRootDir: dirname,
     },
   },
   settings: {
     'import/resolver': {
-      typescript: true,
+      typescript: {
+        project: `${dirname}/tsconfig.json`,
+      },
     },
   },
 });
